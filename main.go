7b838b5a713ef79d2ec3ccb7c47f1864661685b3
sakshi-kulkarni <kulkarnisakshi2410@gmail.com>
2024-11-04 15:46:11 +0530

    chore: added line

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
- 
 	"log"
 	"net/http"
-    
-	"GO-TRAINING/handler"
 
+	"GO-TRAINING/handler"
 )
 
-func main(){
+func main() {
 	http.HandleFunc("/average", handler.AverageHandler)
 	log.Println("server started at :9901")
-	log.Fatal(http.ListenAndServe(":9901",nil))
+	log.Fatal(http.ListenAndServe(":9901", nil))
+	log.Println("")
 }
-
- 
- 
